839395edb88cd9aa6cb6da5a910707f1051903d1
webpawan <pawansaini66477@gmail.com>
2022-10-09 21:46:14 +0530

    completed

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ let flag =true;
 const apidata = async (cityName)  =>{
 try {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=7f8a06848c6c6bf8e0b5f4b66005c349`;
-    // let mapurl = `http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}?appid=7f8a06848c6c6bf8e0b5f4b66005c349`
 let apidata = await fetch(url);
 let weatherdata = await apidata.json();
+let mapshow = document.getElementById('mymap');
 document.getElementById('temp').innerHTML = `${weatherdata.main.temp}&#176;c`;
 document.getElementById('city').innerHTML = `city - ${weatherdata.name}`;
-document.getElementById('max').innerHTML = ` max - ${weatherdata.main.temp_max}`;
-document.getElementById('min').innerHTML = `min - ${weatherdata.main.temp_min}`;
-console.log(weatherdata);
+mapshow.innerHTML = `<iframe class="px-2" style='height: 50vh; width: 100%;' frameborder="0" scrolling="no" src="https://maps.google.com/maps?q=${weatherdata.name}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>`
+document.getElementById('max').innerHTML = ` max -> ${weatherdata.main.temp_max}`;
+document.getElementById('min').innerHTML = ` min -> ${weatherdata.main.temp_min}`;
 } catch (error) {
  console.log(error);
 }
